322460311a071a1598f9e43c87d55cab6d974994
Dan Lash <danlash@gmail.com>
2012-06-06 16:36:21 -0400

    correct SMTP response codes; parse messages with mail gem

--- lib/mini-smtp-server/mini-smtp-server.rb
+++ lib/mini-smtp-server/mini-smtp-server.rb
@@ -1,46 +1,49 @@
 require 'gserver'
+require 'mail'
 
 class MiniSmtpServer < GServer
 
   def initialize(port = 2525, host = "127.0.0.1", max_connections = 4, *args)
     super(port, host, max_connections, *args)
   end
-  
-  def serve(io)
+
+  def reset_message_state
     Thread.current[:data_mode] = false
     Thread.current[:message] = {:data => ""}
+  end
+
+  def serve(io)
+    reset_message_state
     Thread.current[:connection_active] = true
     io.print "220 hello\r\n"
     loop do
       if IO.select([io], nil, nil, 0.1)
-	      data = io.readpartial(4096)
-	      log("<<< " + data) if(@audit)
-	      output = process_line(data)
+        data = io.readpartial(4096)
+        log("<<< " + data) if(@audit)
+        output = process_line(data)
         log(">>> " + output) if(@audit && !output.empty?)
-	      io.print(output) unless output.empty?
+        io.print(output) unless output.empty?
       end
       break if(!Thread.current[:connection_active] || io.closed?)
     end
     io.print "221 bye\r\n"
     io.close
-    Thread.current[:message][:data].gsub!(/\r\n\Z/, '').gsub!(/\.\Z/, '')
-    new_message_event(Thread.current[:message])
   end
 
   def process_line(line)
     # Handle specific messages from the client
     case line
     when (/^(HELO|EHLO)/)
-      return "220 go on...\r\n"
+      return "250 go on...\r\n"
     when (/^QUIT/)
       Thread.current[:connection_active] = false
       return ""
     when (/^MAIL FROM\:/)
       Thread.current[:message][:from] = line.gsub(/^MAIL FROM\:/, '').strip
-      return "220 OK\r\n"
+      return "250 OK\r\n"
     when (/^RCPT TO\:/)
       Thread.current[:message][:to] = line.gsub(/^RCPT TO\:/, '').strip
-      return "220 OK\r\n"
+      return "250 OK\r\n"
     when (/^DATA/)
       Thread.current[:data_mode] = true
       return "354 Enter message, ending with \".\" on a line by itself\r\n"
@@ -51,8 +54,11 @@ class MiniSmtpServer < GServer
     # are being told to exit data mode
     if((Thread.current[:data_mode]) && (line.chomp =~ /^\.$/))
       Thread.current[:message][:data] += line
-      Thread.current[:data_mode] = false
-      return "220 OK\r\n"
+
+      new_message_event(Mail.read_from_string(Thread.current[:message][:data]))
+
+      reset_message_state
+      return "250 OK\r\n"
     end
     
     # If we are in date mode then we need to add
